7585e2b0482d02c115d19225a4dd43050d61fc18
Xavier Pillons (MSFT) <xpillons@microsoft.com>
2025-02-25 16:19:36 +0100

    dynamic configuration

--- specs/scheduler/cluster-init/files/slurm_proxy.sh
+++ specs/scheduler/cluster-init/files/slurm_proxy.sh
@@ -3,7 +3,7 @@ SLURM_CMD=$1
 shift
 
 ssh_options="-o StrictHostKeyChecking=no -o LogLevel=quiet -o UserKnownHostsFile=/dev/null -o GlobalKnownHostsFile=/dev/null"
-ssh ccw-login-1 $ssh_options "$SLURM_CMD $@"
+ssh __SCHEDULER__ $ssh_options "$SLURM_CMD $@"
 EXIT_CODE=$(echo $?)
 
 exit $EXIT_CODE
--- specs/scheduler/cluster-init/scripts/01-configure.sh
+++ specs/scheduler/cluster-init/scripts/01-configure.sh
@@ -10,14 +10,26 @@ mkdir -pv $OOD_CLUSTER_DIR
 # - Retrieve the cluster name and login node names
 # - Update clusters definition files
 # - Copy clusters definition files to the shared directory
-CLUSTER_NAME="ccw"
+CLUSTER_NAME=$(jetpack config cyclecloud.cluster.name)
+NODE_PREFIX=$(jetpack config jetpack config slurm.node_prefix)
+BIN_OVERRIDES=$OOD_CLUSTER_DIR/slurm_$CLUSTER_NAME/bin_overrides
 
 # Copy clusters definition files to the shared directory
 cp -fv ${files_dir}/login_cluster.yml $OOD_CLUSTER_DIR/login_slurm_$CLUSTER_NAME.yml
 cp -fv ${files_dir}/slurm_cluster.yml $OOD_CLUSTER_DIR/slurm_$CLUSTER_NAME.yml
 
+sed -i "s/__CLUSTER_NAME__/${CLUSTER_NAME}/g" $OOD_CLUSTER_DIR/slurm_$CLUSTER_NAME.yml
+sed -i "s/__BIN_OVERRIDES_DIR__/${BIN_OVERRIDES}/g" $OOD_CLUSTER_DIR/slurm_$CLUSTER_NAME.yml
+sed -i "s/__NODE_PREFIX__/${NODE_PREFIX}/g" $OOD_CLUSTER_DIR/slurm_$CLUSTER_NAME.yml
+sed -i "s/__NODE_PREFIX__/${NODE_PREFIX}/g" $OOD_CLUSTER_DIR/login_slurm_$CLUSTER_NAME.yml
+
 # Copy slurm command wrapper scripts to the shared directory
-mkdir -pv $OOD_CLUSTER_DIR/slurm_$CLUSTER_NAME/bin_overrides
-cp -fv "${files_dir}/slurm_proxy.sh" "${files_dir}/sacctmgr.sh" "${files_dir}/sbatch.sh" "${files_dir}/scancel.sh" "${files_dir}/scontrol.sh" "${files_dir}/sinfo.sh" "${files_dir}/squeue.sh" $OOD_CLUSTER_DIR/slurm_$CLUSTER_NAME/bin_overrides
-chmod +x $OOD_CLUSTER_DIR/slurm_$CLUSTER_NAME/bin_overrides/*.sh
+mkdir -pv $BIN_OVERRIDES
+cp -fv "${files_dir}/slurm_proxy.sh" "${files_dir}/sacctmgr.sh" "${files_dir}/sbatch.sh" "${files_dir}/scancel.sh" "${files_dir}/scontrol.sh" "${files_dir}/sinfo.sh" "${files_dir}/squeue.sh" $BIN_OVERRIDES
+
+# Update the scheduler name in the slurm proxy script
+SCHEDULER=$(hostname)
+sed -i "s/__SCHEDULER__/${SCHEDULER}/g" $BIN_OVERRIDES/slurm_proxy.sh
+
+chmod +x $BIN_OVERRIDES/*.sh
 
