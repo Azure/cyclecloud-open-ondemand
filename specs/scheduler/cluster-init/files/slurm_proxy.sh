a54465c9623cb8bd5eb86121df4c0b64b044340f
Xavier Pillons (MSFT) <xpillons@microsoft.com>
2025-02-25 15:41:03 +0100

    set proxy node

--- specs/scheduler/cluster-init/files/slurm_proxy.sh
+++ specs/scheduler/cluster-init/files/slurm_proxy.sh
@@ -3,7 +3,7 @@ SLURM_CMD=$1
 shift
 
 ssh_options="-o StrictHostKeyChecking=no -o LogLevel=quiet -o UserKnownHostsFile=/dev/null -o GlobalKnownHostsFile=/dev/null"
-ssh {{ Login_ip }} $ssh_options "$SLURM_CMD $@"
+ssh ccw-login-1 $ssh_options "$SLURM_CMD $@"
 EXIT_CODE=$(echo $?)
 
 exit $EXIT_CODE
