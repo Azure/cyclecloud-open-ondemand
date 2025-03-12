3a25388c8344f9a7aea263d17f8dabbb248538d7
Xavier Pillons (MSFT) <xpillons@microsoft.com>
2025-03-12 15:51:01 +0100

    move register_cluster cron into register_cluster script

--- specs/default/cluster-init/scripts/02-Register-Clusters.sh
+++ specs/default/cluster-init/scripts/02-Register-Clusters.sh
@@ -5,3 +5,12 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Register Clusters
 $script_dir/../files/install.sh register_cluster
+
+#write out current crontab
+crontab -l > tempcron
+#echo new cron into cron file
+echo "0,10,20,30,40,50 * * * *  rm -rf /mnt/cluster-init/ood/default/files/playbooks/register_cluster.ok && /mnt/cluster-init/ood/default/files/install register_cluster >> /opt/cycle/jetpack/logs/register_cluster.out 1>&2" >> tempcron
+#install new cron file
+crontab tempcron
+
+rm tempcron
