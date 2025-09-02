62209133e5f9d077e6094f030304fc519581966c
Xavier Pillons (MSFT) <xpillons@microsoft.com>
2025-09-02 18:28:34 +0200

    remove extra miniconda

--- specs/default/cluster-init/files/python_wrapper.sh
+++ specs/default/cluster-init/files/python_wrapper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source ${THIS_DIR}/oodenv/bin/activate oodenv
+source ${THIS_DIR}/oodenv/bin/activate
 
 exec /bin/env python3 "$@"
